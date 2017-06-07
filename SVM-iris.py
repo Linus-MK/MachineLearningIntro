60fdc85ba76816d4df48fc035f124c6c697e3b56
Linus-MK <murmur2winzcud37dle@gmail.com>
2017-06-07 23:33:02 +0900

    scikit-learnのSVMを写経

---
+++ SVM-iris.py
@@ -0,0 +1,15 @@
+# 「データサイエンティスト養成読本　機械学習入門編」P.131より
+
+
+from sklearn import datasets
+from sklearn import svm
+from sklearn import cross_validation
+
+
+iris = datasets.load_iris()
+
+svc = svm.SVC()
+scores = cross_validation.cross_val_score(svc, iris.data, iris.target, cv=5)
+
+print(scores)
+print("Accuracy:", scores.mean() )
